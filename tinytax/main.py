796e816ce13ec7728652ae24c8fb5b6a2043fee2
Joshua Leffert <jleffert9@gmail.com>
2022-01-20 23:22:53 -0600

    Clean up comments

--- tinytax/main.py
+++ tinytax/main.py
@@ -4,5 +4,3 @@ from wallet import Wallet
 wallet = Wallet(input('Enter Wallet Address: '))
 print(wallet.get_address())
 transactions = wallet.get_transactions(50)
-
-# wallet = str(input('Paste wallet ID and press Enter: '))
--- tinytax/transaction.py
+++ tinytax/transaction.py
@@ -8,8 +8,10 @@ class Transaction:
         self.date = str(datetime.fromtimestamp(transaction_data['round-time']))
         if 'group' in transaction_data:
             self.group_id = transaction_data['group']
+        
         if transaction_data['sender'] == self.wallet.address:
             self.fee = transaction_data['fee']
         else:
+            # No fees when user is not sender
             self.fee = 0
 
