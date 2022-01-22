a14ef9c9d81e469c14e9c65cb9417e316b2a321a
Joshua Leffert <jleffert9@gmail.com>
2022-01-21 23:25:59 -0600

    Update main.py

--- tinytax/main.py
+++ tinytax/main.py
@@ -1,6 +1,4 @@
-from contracts import yieldlyDB, algofiDB
 from wallet import Wallet
 
 wallet = Wallet(input('Enter Wallet Address: '))
-print(wallet.get_address())
 transactions = wallet.get_transactions(50)
