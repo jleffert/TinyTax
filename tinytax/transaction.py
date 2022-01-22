c861bf59b29a946339f141ab8605b9f1e5ed336c
Joshua Leffert <jleffert9@gmail.com>
2022-01-21 22:40:58 -0600

    Stub out transaction sub classes
    Stub out transaction sub classes and clean up variable names.

--- tinytax/transaction.py
+++ tinytax/transaction.py
@@ -1,17 +1,67 @@
 from datetime import datetime
 
+TRANSACTION_TYPES = {
+    'pay' : 'ALGO Transaction',
+    'axfer' : 'ASA Transaction',
+    'appl' : 'Application Transaction'
+}
+
 class Transaction:
-    def __init__(self, wallet, transaction_data):
+    def __init__(self, wallet, data):
         self.wallet = wallet
-        self.id = transaction_data['id']
-        self.sender = transaction_data['sender']
-        self.date = str(datetime.fromtimestamp(transaction_data['round-time']))
-        if 'group' in transaction_data:
-            self.group_id = transaction_data['group']
+        self.id = data['id']
+        self.sender = data['sender']
+        self.date = str(datetime.fromtimestamp(data['round-time']))
+        if 'group' in data:
+            self.group_id = data['group']
         
-        if transaction_data['sender'] == self.wallet.address:
-            self.fee = transaction_data['fee']
+        if data['sender'] == self.wallet.address:
+            self.fee = data['fee']
         else:
             # No fees when user is not sender
             self.fee = 0
+        
+        self.transaction_type = data['tx-type']
+        self.initalize_amount(data)
 
+    def readable_transaction_type(self):
+        if self.transaction_type in TRANSACTION_TYPES:
+            return TRANSACTION_TYPES[self.transaction_type]
+        else:
+            return 'Unknown'
+
+    def initalize_amount(self, data):
+        match self.transaction_type:
+            case 'pay':
+                pass
+            case 'axfer':
+                pass
+            case 'appl':
+                pass
+
+class PaymentTransaction(Transaction):
+    def __init__(self, wallet, data):
+        super().__init__(wallet, data)
+        self.transaction_type = 'pay'
+        print('pay')
+
+class AsaTransaction(Transaction):
+    def __init__(self, wallet, data):
+        super().__init__(wallet, data)
+        self.transaction_type = 'axfer'
+        print('axfer')
+
+class ApplicationTransaction(Transaction):
+    def __init__(self, wallet, data):
+        super().__init__(wallet, data)
+        self.transaction_type = 'appl'
+        print('appl')
+
+def transaction_builder(wallet, data):
+    match data['tx-type']:
+        case 'pay':
+            return PaymentTransaction(wallet, data)
+        case 'axfer':
+            return AsaTransaction(wallet, data)
+        case 'appl':
+            return ApplicationTransaction(wallet, data)
--- tinytax/wallet.py
+++ tinytax/wallet.py
@@ -1,5 +1,5 @@
 from requests import get
-from transaction import Transaction
+from transaction import transaction_builder
 from transaction_group import TransactionGroup
 ACCOUNT_URL = 'https://algoindexer.algoexplorerapi.io/v2/accounts/'
 
@@ -27,7 +27,7 @@ class Wallet:
         
         while 'next-token' in response_json:
             for transaction_json in response_json['transactions']:
-                transaction = Transaction(self, transaction_json)
+                transaction = transaction_builder(self, transaction_json)
                 if 'group' in transaction_json:
                     self.add_to_transaction_group(transaction)
                 self.transactions.append(transaction)
@@ -37,7 +37,6 @@ class Wallet:
 
     def add_to_transaction_group(self, transaction):
         if transaction.group_id not in self.transaction_group:
-            print(f"New Group {transaction.group_id}")
             self.transaction_group[transaction.group_id] = TransactionGroup(transaction.group_id)
         
         self.transaction_group[transaction.group_id].transactions.append(transaction)
