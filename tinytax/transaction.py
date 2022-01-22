0d59158a65d5dd13970975696c15120d095b23bb
Joshua Leffert <jleffert9@gmail.com>
2022-01-21 23:27:46 -0600

    Initialize payment transactions

--- tinytax/transaction.py
+++ tinytax/transaction.py
@@ -21,29 +21,33 @@ class Transaction:
             # No fees when user is not sender
             self.fee = 0
         
-        self.transaction_type = data['tx-type']
-        self.initalize_amount(data)
-
     def readable_transaction_type(self):
         if self.transaction_type in TRANSACTION_TYPES:
             return TRANSACTION_TYPES[self.transaction_type]
         else:
             return 'Unknown'
 
-    def initalize_amount(self, data):
-        match self.transaction_type:
-            case 'pay':
-                pass
-            case 'axfer':
-                pass
-            case 'appl':
-                pass
 
 class PaymentTransaction(Transaction):
     def __init__(self, wallet, data):
         super().__init__(wallet, data)
         self.transaction_type = 'pay'
-        print('pay')
+        payment_data = data['payment-transaction']
+        self.receiver = payment_data['receiver']
+        if payment_data['amount'] != 0: # 0 Transactions are for compounding rewards
+            if self.receiver == wallet.address:
+                self.type = 'Receive'
+                self.receive_quantity = payment_data['amount'] 
+                self.receive_asset_id = 'ALGO'
+            else:
+                self.type = 'Send'
+                self.out_quantity = payment_data['amount']
+                self.out_asset_id = 'ALGO'
+        elif self.sender == wallet.address and self.receiver == wallet.address:
+            self.type = 'Staking'
+        else:
+            self.type = 'Unkown Payment'
+    
 
 class AsaTransaction(Transaction):
     def __init__(self, wallet, data):
