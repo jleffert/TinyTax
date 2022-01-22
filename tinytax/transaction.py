832964b46c6c3e366b9875a37da359d7915b4d84
Joshua Leffert <jleffert9@gmail.com>
2022-01-21 23:41:52 -0600

    Initialize ASA transactions

--- tinytax/transaction.py
+++ tinytax/transaction.py
@@ -1,3 +1,4 @@
+from ctypes import addressof
 from datetime import datetime
 from contracts import yieldlyDB, algofiDB
 
@@ -13,6 +14,8 @@ class Transaction:
         self.id = data['id']
         self.sender = data['sender']
         self.date = str(datetime.fromtimestamp(data['round-time']))
+        self.platform = None
+
         if 'group' in data:
             self.group_id = data['group']
         
@@ -63,8 +66,31 @@ class AsaTransaction(Transaction):
     def __init__(self, wallet, data):
         super().__init__(wallet, data)
         self.transaction_type = 'axfer'
-        self.receiver = data['asset-transfer-transaction']['receiver']
-        self.set_platform()
+        asset_transfer_data = data['asset-transfer-transaction']
+        self.receiver = asset_transfer_data['receiver']
+        
+        asset_id = str(asset_transfer_data['asset-id'])
+        if 'close-to' in asset_transfer_data: # Remove ASA
+            self.type = 'Close ASA'
+            self.out_asset_id = asset_id
+            self.platform = asset_id
+        elif self.sender == wallet.address:
+            if self.receiver == wallet.address:
+                self.type = 'Sign for ASA'
+                self.in_asset_id = asset_id
+            else:
+                self.type = 'Send'
+                self.out_quantity = asset_transfer_data['amount']
+                self.out_asset_id = asset_id
+        elif self.sender is not wallet.address and self.receiver == wallet.address:
+            self.type = 'Receive'
+            self.in_quantity = asset_transfer_data['amount']
+            self.in_asset_id = asset_id
+        else:
+            self.type = 'Unkown ASA Transaction'
+
+        if self.platform is not None:
+            self.set_platform()
 
 class ApplicationTransaction(Transaction):
     def __init__(self, wallet, data):
