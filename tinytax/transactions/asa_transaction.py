38b30c2c0c9545f65ddd53fd652807d70e18970d
Joshua Leffert <jleffert9@gmail.com>
2022-01-26 22:26:18 -0600

    Build ASA dictionary as you go

--- tinytax/transactions/asa_transaction.py
+++ tinytax/transactions/asa_transaction.py
@@ -1,4 +1,44 @@
+from requests import get
+from string import digits
+import re
 from transactions.transaction import Transaction
+
+ASA_URL = 'https://algoindexer.algoexplorerapi.io/v2/assets/'
+TINYMAN_VERSION_REGEX = "Tinyman\s?Pool([0-9]+\.[0-9])? .*"
+TINYMAN_TICKER_REGEX = "Tinyman\s?Pool.* ([A-Z]+-[A-Z]+)"
+asa_dictionary = {}
+
+# TODO: Put in own class
+def build_asset(asset_id):
+    response = get(ASA_URL + str(asset_id))
+    data = response.json()
+    if 'asset' not in data:
+        asa_dictionary[asset_id] = { 'name' : asset_id, 'ticker' : asset_id, 'decimals' : 0 }
+    else:
+        asset = {}
+        asset_data = data['asset']
+        params = asset_data['params']
+        asset['decimals'] = params['decimals']
+        if 'unit-name' in params:
+            name_with_no_digits = params['unit-name'].translate({ord(k): None for k in digits}) 
+            # Detect tinyman both v1 and v1.1
+            if name_with_no_digits == 'TMPOOL':
+                asset['ticker'] = params['name']
+                version = re.findall(TINYMAN_VERSION_REGEX, params['name'])[0]
+                if version != '':
+                    asset['tinyman_version'] = version
+                else:
+                    asset['tinyman_version'] = '1'
+                asset['ticker'] = re.findall(TINYMAN_TICKER_REGEX, params['name'])[0]
+                asa_dictionary[asset_id] = asset['tinyman_version']
+            else:
+                asset['ticker'] = params['unit-name']
+        else:
+            asset['ticker'] = asset_id
+        if 'name' in params:
+            asset['name'] = params['name']
+        
+        asa_dictionary[asset_id] = asset
 
 class AsaTransaction(Transaction):
     def __init__(self, wallet, data):
@@ -30,4 +70,6 @@ class AsaTransaction(Transaction):
 
         if self.platform is None:
             self.set_platform()
-        
+
+        if asset_id not in asa_dictionary:
+            build_asset(asset_id)                
