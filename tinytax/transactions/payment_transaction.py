c8851f8f7bb456cdffeab1cea4377a23e50557c8
Joshua Leffert <jleffert9@gmail.com>
2022-01-26 23:52:56 -0600

    Divide ALGO transaction values by 6 decimal points

--- tinytax/transactions/payment_transaction.py
+++ tinytax/transactions/payment_transaction.py
@@ -7,17 +7,21 @@ class PaymentTransaction(Transaction):
         payment_data = data['payment-transaction']
         self.receiver = payment_data['receiver']
         self.set_rewards(data)
+
         if payment_data['amount'] != 0: # 0 Transactions are for compounding rewards
             if self.receiver == wallet.address:
                 self.type = 'Receive'
-                self.receive_quantity = payment_data['amount'] 
-                self.receive_asset_id = 'ALGO'
+                self.in_quantity = float(payment_data['amount']) / 1000000
+                self.in_asset_id = 'ALGO'
             else:
                 self.type = 'Send'
-                self.out_quantity = payment_data['amount']
+                self.out_quantity = float(payment_data['amount']) / 1000000
                 self.out_asset_id = 'ALGO'
         elif self.sender == wallet.address and self.receiver == wallet.address:
             self.type = 'Staking'
         else:
             self.type = 'Unkown Payment'
         self.set_platform()
+        
+        if self.fee > 0:
+            self.fee = self.fee / 1000000
